332417b83c5b63f14c9fa0949f1a6cfedcc6216b
Tomasz Sielski <tom.sielski.pl@gmail.com>
2025-10-02 14:43:58 +0200

    implemented authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { useMsal } from "@azure/msal-react";
 
 function App() {
-  return <h1>Hello, Friendly Happiness! second attempt</h1>;
+  const { instance, accounts } = useMsal();
+
+  const handleLogin = () => {
+    instance.loginPopup();
+  };
+
+  const handleLogout = () => {
+    instance.logoutPopup();
+  };
+
+  return (
+    <div>
+      {accounts.length > 0 ? (
+        <>
+          <h1>Welcome, {accounts[0].username}</h1>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      ) : (
+        <button onClick={handleLogin}>Login with Microsoft</button>
+      )}
+    </div>
+  );
 }
 
 export default App;
