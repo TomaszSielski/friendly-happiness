99ba4773e668dece748cf97a996a4aa708a21fdb
Tomasz Sielski <tom.sielski.pl@gmail.com>
2025-10-02 14:50:10 +0200

    correct redirect url

--- frontend/src/authConfig.js
+++ frontend/src/authConfig.js
@@ -2,6 +2,6 @@ export const msalConfig = {
   auth: {
     clientId: process.env.REACT_APP_CLIENT_ID,
     authority: `https://login.microsoftonline.com/${process.env.REACT_APP_TENANT_ID}`,
-    redirectUri: "http://localhost:3000",
+    redirectUri: "https://stunning-phoenix-a34b0a.netlify.app/",
   },
 };
