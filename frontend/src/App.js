66fe954a0278ad59d7267a5f6d4c883a224c15ac
Tomasz Sielski <tom.sielski.pl@gmail.com>
2025-10-03 16:09:34 +0200

    login expirience improvement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useMsal } from "@azure/msal-react";
+import Spinner from "./components/Spinner";
 
 function App() {
   const { instance, accounts } = useMsal();
+  const [loading, setLoading] = useState(true);
 
-  const handleLogin = () => {
-    instance.loginPopup();
+  useEffect(() => {
+    const accounts = instance.getAllAccounts();
+    setLoading(false);
+  }, [instance]);
+
+  const handleLogin = async () => {
+    try {
+      await instance.loginPopup();
+    } catch (error) {
+      if (error.errorCode === "user_cancelled") {
+        console.log("User cancelled the login.");
+      } else console.error("Login failed:", error);
+    }
   };
 
   const handleLogout = () => {
     instance.logoutPopup();
   };
 
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
     <div>
+      <h1>Welcome to Friendly Happiness</h1>
+      <p>Your journey to happiness starts here.</p>
       {accounts.length > 0 ? (
         <>
           <h1>Welcome, {accounts[0].username}</h1>
